8c248585bb491626dfe7aa0a7aac5578adf31092
Tuan Anh <tanh1@facebook.com>
2016-01-15 21:56:33 -0800

    Initial Commit

---
+++ Malloc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Malloc
+//
+//  Created by Anh Nguyen on 1/15/16.
+//  Copyright © 2016 Anh Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
